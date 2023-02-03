39337f3a344c3d0ef69699c5a261e8a83b7e765c
Triston Li <76667945+triston-li@users.noreply.github.com>
2023-02-03 01:58:59 -0500

    Add files via upload
    First self written Python script from enki lesson prompt.

---
+++ first self written.py
@@ -0,0 +1,6 @@
+print ("Type a number and I'll tell you if it's even or odd!")
+number = input("Your Number:")
+if int(number)%2 == 0:
+  print("Your number is even!")
+else:
+  print("Your number is odd!")
