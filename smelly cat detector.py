f94ccbd625f00ce8f40e0a2a6c4be421cd3eb43d
Triston Li <76667945+triston-li@users.noreply.github.com>
2023-02-03 02:16:04 -0500

    Add files via upload
    Smelly cat detector made for enki prompt

---
+++ smelly cat detector.py
@@ -0,0 +1,8 @@
+names = "stinky"
+print("What is your name?")
+username = input("Type your name:")
+
+if names in username:
+  print("I knew it was you, smelly cat!")
+else:
+  print("Thank goodness you're not that smelly cat!")
